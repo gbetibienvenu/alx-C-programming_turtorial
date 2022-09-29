c8bf287fc184ecd60e679fdcbb5d1c56d0aa8ee1
gbetibienvenu <64703165+gbetibienvenu@users.noreply.github.com>
2022-09-29 08:39:51 +0100

    Add files via upload
    This is the code for pratice under this repository for this turtorial

---
+++ 03_type.py
@@ -0,0 +1,18 @@
+a = "This is a"
+b = 34
+c = None
+d = True
+Marcus = 44.5
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+print(type(Marcus))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
---
+++ 04_string_functions.py
@@ -0,0 +1,14 @@
+myStr = "abcdefghijklmnopqrstuvwxyzaaab"
+marcus = "abcd"
+
+print(len(marcus))
+
+print(len(myStr))
+print(myStr.endswith("xyz"))
+print(myStr.startswith("abcd"))
+print(myStr.count('ab'))
+
+myStr = "my name is harry name"
+print(myStr.capitalize())
+print(myStr.find("name"))
+print(myStr.replace("name", "date"))
